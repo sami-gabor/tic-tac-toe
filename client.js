e6607a146724b93b9e0d0d3dd7bf0239d1655fd4
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-05-30 14:05:42 +0300

    initial

---
+++ client.js
@@ -0,0 +1,116 @@
+const ioClient = io.connect('http://localhost:8000');
+
+
+function createTable(arr) {
+  const $container = document.getElementById('container');
+  const $table = document.createElement('table');
+  const $tableBody = document.createElement('tbody');
+  arr.forEach((row, rowIndex) => {
+    const $tableRow = document.createElement('tr');
+    row.forEach((col, colIndex) => {
+      const $tableData = document.createElement('td');
+      $tableData.setAttribute('id', `${rowIndex}${colIndex}`);
+      $tableData.appendChild(document.createTextNode(col));
+      $tableRow.appendChild($tableData);
+    });
+    $tableBody.appendChild($tableRow);
+  });
+  $table.appendChild($tableBody);
+  $container.innerHTML = '';
+  $container.appendChild($table);
+}
+
+
+const updateMessageField = (message) => {
+  const $message = document.getElementById('messages');
+  $message.innerHTML = message;
+};
+
+
+const updateGameBoard = (cellIndex, currentPlayerSelection) => {
+  const $cell = document.getElementById(cellIndex);
+  $cell.innerHTML = currentPlayerSelection;
+};
+
+
+const handleGameClick = (e) => {
+  const cellIndex = e.target.id;
+  ioClient.emit('gameInput', cellIndex);
+};
+
+
+const freezeBoardGame = () => {
+  const $boardGame = document.querySelector('tbody');
+  $boardGame.removeEventListener('click', handleGameClick);
+};
+
+
+// create a room and automatically add the player to it
+const $createGameRoom = document.getElementById('new');
+$createGameRoom.addEventListener('click', () => {
+  const playerName = document.getElementById('nameNew').value;
+  ioClient.emit('new', playerName);
+});
+
+// send the player name(from the input field) to server
+const $joinGameButton = document.getElementById('join');
+$joinGameButton.addEventListener('click', () => {
+  const playerName = document.getElementById('nameJoin').value;
+  const roomId = document.getElementById('room').value;
+  ioClient.emit('join', playerName, roomId);
+});
+
+
+const addGameInputListener = () => {
+  // send the game input(from the board game) to server
+  const $boardGame = document.querySelector('tbody');
+  $boardGame.addEventListener('click', handleGameClick);
+};
+
+const addNewRoom = (roomId) => {
+  const $roomsList = document.getElementById('rooms');
+  const $room = document.createElement('li');
+  $room.appendChild(document.createTextNode(roomId));
+  $roomsList.appendChild($room);
+};
+
+
+// set up incomming communication channels
+ioClient.on('connect', () => {
+  console.log('Client has connected to the server!');
+});
+
+ioClient.on('room created', (roomId) => {
+  addNewRoom(roomId);
+});
+
+ioClient.on('startGame', (emptyMatrix) => {
+  createTable(emptyMatrix);
+  addGameInputListener();
+});
+
+ioClient.on('updateGame', (cellIndex, cellValue) => {
+  updateGameBoard(cellIndex, cellValue);
+});
+
+ioClient.on('gameOver', (message) => {
+  updateMessageField(message);
+  freezeBoardGame();
+});
+
+ioClient.on('freeze game', (message) => {
+  updateMessageField(message);
+  freezeBoardGame();
+});
+
+ioClient.on('unfreeze game', (message) => {
+  updateMessageField(message);
+  addGameInputListener();
+});
+
+
+ioClient.on('message', (message) => {
+  updateMessageField(message);
+});
+
+ioClient.on('disconnect', () => console.log('The server has disconnected!'));
---
+++ server.js
@@ -0,0 +1,205 @@
+const generateMatrix = (rows = 3, columns = 3) => {
+  const matrix = [];
+
+  const createRow = cols => new Array(cols).fill('');
+
+  for (let i = 0; i < rows; i += 1) {
+    matrix.push(createRow(columns));
+  }
+
+  return matrix;
+};
+const matrix = generateMatrix(3, 3);
+
+
+const checkRow = (rowIndex, currentSelection) => {
+  const selections = [];
+
+  matrix[rowIndex].forEach((col) => {
+    if (col === currentSelection) {
+      selections.push(col);
+    }
+  });
+
+  return selections.length === matrix.length;
+};
+
+
+const checkCol = (colIndex, currentSelection) => {
+  const selections = [];
+
+  matrix.forEach((row) => {
+    const cellToCheck = row[colIndex];
+    if (cellToCheck === currentSelection) {
+      selections.push(cellToCheck);
+    }
+  });
+
+  return selections.length === matrix.length;
+};
+
+
+const checkPrincipalDiagonal = (currentSelection) => {
+  const selections = [];
+
+  matrix.forEach((row, index) => {
+    const cellToCheck = row[index];
+    if (cellToCheck === currentSelection) {
+      selections.push(cellToCheck);
+    }
+  });
+
+  return selections.length === matrix.length;
+};
+
+
+const checkSecondaryDiagonal = (currentSelection) => {
+  const selections = [];
+
+  matrix.forEach((row, index) => {
+    const cellToCheck = row[matrix.length - index - 1];
+    if (cellToCheck === currentSelection) {
+      selections.push(cellToCheck);
+    }
+  });
+
+  return selections.length === matrix.length;
+};
+
+
+const checkWinner = (cellIndex, currentSelection) => {
+  const [rowIndex, colIndex] = cellIndex;
+
+  if (checkRow(rowIndex, currentSelection)
+    || checkCol(colIndex, currentSelection)
+    || ((rowIndex === colIndex) && checkPrincipalDiagonal(currentSelection))
+    || ((parseInt(rowIndex, 10) + parseInt(colIndex, 10) === matrix.length - 1) && checkSecondaryDiagonal(currentSelection))) {
+    return true;
+  }
+
+  return false;
+};
+
+
+const updateMatrix = ([rowIndex, colIndex], clientSelection) => {
+  // const [rowIndex, colIndex] = index;
+  matrix[rowIndex][colIndex] = clientSelection;
+};
+
+
+// handle communication channels with clients
+const io = require('socket.io');
+
+const server = io.listen(8000);
+let currentMoveIsX = true;
+let movesCount = 0;
+// will store the name, ID for every player
+const players = [];
+const rooms = [];
+
+const addPlayer = (name, socket) => {
+  players.push({ name, id: socket.id });
+  console.log(`${socket.id} was succesfully changed to "${name}".`);
+};
+
+const validName = (name) => {
+  for (let i = 0; i < players.length; i += 1) {
+    if (players[i].name === name) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const startGame = (socket) => {
+  // broadcast back to sender
+  socket.emit('startGame', matrix);
+  socket.emit('freeze game', `Wait for ${players[0].name} to move.`);
+  // broadcast to everyone except the sender
+  socket.broadcast.emit('startGame', matrix);
+  socket.broadcast.emit('message', 'It\' your turn!');
+};
+
+const createRoom = (playerName, roomId) => {
+  rooms.push({ roomId, player1: playerName, player2: 'none' });
+};
+
+const emitCreateRoomConfirmation = (socket) => {
+  socket.emit('room created', socket.id);
+}
+
+
+server.on('connection', (socket) => {
+  const playerId = socket.id;
+
+  socket.on('new', (playerName) => {
+    createRoom(playerName, socket.id);
+    emitCreateRoomConfirmation(socket);
+  });
+
+  socket.on('join', (playerName) => {
+    // check the number of players hwo joined the game
+    switch (players.length) {
+      case 0:
+        addPlayer(playerName, playerId);
+        socket.emit('message', 'Waiting for the second player to join.');
+        break;
+
+      case 1:
+        if (validName(playerName)) {
+          addPlayer(playerName, socket);
+          startGame(socket);
+        } else {
+          socket.emit('message', 'This name is already in use. Try something else.');
+        }
+        break;
+
+      default:
+        socket.emit('message', 'Too many players');
+    }
+  });
+
+
+  // verify if the message reveived(obj) has the cellIndex property
+  socket.on('gameInput', (cellIndex) => {
+    let winner;
+    movesCount += 1;
+    // determine what the current cellValue is and update the matrix with it
+    const cellValue = currentMoveIsX ? 'X' : '0';
+    updateMatrix(cellIndex, cellValue);
+    currentMoveIsX = !currentMoveIsX;
+
+    socket.emit('freeze game', 'wait a sec...');
+    socket.broadcast.emit('unfreeze game', 'It\'s your turn!');
+
+    socket.broadcast.emit('updateGame', cellIndex, cellValue); // broadcast message to everyone except the sender
+    socket.emit('updateGame', cellIndex, cellValue); // broadcast message back to sender
+
+    if (checkWinner(cellIndex, cellValue)) {
+      winner = cellValue;
+      socket.broadcast.emit('gameOver', `Player ${winner} won!`);
+      socket.emit('gameOver', `Player ${winner} won!`);
+    } else if (movesCount === 9) {
+      socket.broadcast.emit('gameOver', 'It\'s a tie. Nobody won!');
+      socket.emit('gameOver', 'It\'s a tie. Nobody won!');
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`disconnected from: ${playerId}`);
+    socket.broadcast.emit('message', `${playerId} was disconnected.`);
+  });
+
+  const standardInput = process.stdin;
+  standardInput.setEncoding('utf-8');
+  standardInput.on('data', (data) => {
+    if (data === 'exit\n') {
+      process.exit();
+    } else {
+      socket.broadcast.emit('message', data); // broadcast the message to everyone except the sender
+      socket.emit('message', { matrix }); // broadcast the message to sender
+    }
+  });
+
+  console.log(`${socket.id} has connected.`);
+});
