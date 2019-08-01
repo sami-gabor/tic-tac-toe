dc4b769c4e3f771764f84d1695ed18743347c33d
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-08-01 14:28:52 +0300

    update .gitignore

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clientID: '8786c74e247240a2377c',
+  clientSecret: 'dc9b7a7beec12a7b108a8173dbb296f5af80df37',
+  callbackURL: 'http://localhost:3000/auth',
+  scope: 'user:email',
+};
