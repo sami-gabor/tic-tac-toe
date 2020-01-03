7216eb88d8d1d541c1e879caa8162b4b1bc4219e
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-01-03 15:34:49 +0200

    reverse auth

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   clientID: '8786c74e247240a2377c',
   clientSecret: 'dc9b7a7beec12a7b108a8173dbb296f5af80df37',
-  callbackURL: 'http://165.22.72.239:3000',
+  callbackURL: 'http://165.22.72.239:3000/auth',
   scope: 'user:email',
 };
---
+++ pull.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -i ~/.ssh/work-private-key.ppk root@165.22.72.239
